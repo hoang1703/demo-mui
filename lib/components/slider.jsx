191eea8747f62434b704d5e3ec07c685ffae49b2
hoang <gvscotcot@gmail.com>
2025-02-12 10:34:42 +0700

    Add thumbs

--- lib/components/slider.jsx
+++ lib/components/slider.jsx
@@ -1,20 +1,22 @@
 "use client";
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper/core";
-import { Navigation, Pagination } from "swiper/modules";
+// import SwiperCore from "swiper/core";
+import { Navigation, FreeMode, Thumbs } from "swiper/modules";
 import { Modal, IconButton, Box } from "@mui/material";
 import { IoDocumentTextOutline } from "react-icons/io5";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "swiper/css/free-mode";
+import "swiper/css/thumbs";
 
 const FileSlider = ({ files }) => {
-  SwiperCore.use([Navigation, Pagination]);
+  // SwiperCore.use([Navigation, Pagination, Thumbs]);
   const [open, setOpen] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   const handleOpen = (file) => {
     setSelectedFile(file);
@@ -31,11 +33,19 @@ const FileSlider = ({ files }) => {
 
     if (["jpg", "jpeg", "png", "gif"].includes(extension)) {
       return (
-        <img
-          src={file.url}
-          alt={file.name}
-          className="h-full w-full object-contain"
-        />
+        <div className="relative">
+          <button
+            className="absolute right-0 top-0"
+            onClick={() => handleOpen(file)}
+          >
+            View
+          </button>
+          <img
+            src={file.url}
+            alt={file.name}
+            className="h-full w-full object-contain"
+          />
+        </div>
       );
     }
 
@@ -65,26 +75,84 @@ const FileSlider = ({ files }) => {
     return null;
   };
 
+  const renderThumbs = (file) => {
+    const extension = file.name.split(".").pop().toLowerCase();
+
+    if (["jpg", "jpeg", "png", "gif"].includes(extension)) {
+      return (
+        <div className="relative">
+          <button className="absolute right-0 top-0">View</button>
+          <img
+            src={file.url}
+            alt={file.name}
+            className="h-full w-full object-contain"
+          />
+        </div>
+      );
+    }
+
+    if (["mp4", "webm", "ogg"].includes(extension)) {
+      return (
+        <video controls className="h-full w-full">
+          <source src={file.url} type={`video/${extension}`} />
+          Your browser does not support the video tag.
+        </video>
+      );
+    }
+
+    if (["pdf", "xml"].includes(extension)) {
+      return (
+        <div
+          className="flex flex-col items-center justify-center h-full"
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            handleOpen(file);
+          }}
+        >
+          <IoDocumentTextOutline />
+        </div>
+      );
+    }
+
+    return null;
+  };
   return (
     <div className="relative" style={{ maxWidth: 600 }}>
       <Swiper
         spaceBetween={30}
         slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        // breakpoints={{
-        //   640: { slidesPerView: 1 },
-        //   768: { slidesPerView: 2 },
-        //   1024: { slidesPerView: 3 },
-        // }}
         className="h-96"
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
       >
         {files.map((file, index) => (
           <SwiperSlide
             key={index}
-            // className="bg-gray-100 rounded-lg overflow-hidden"
+            className="bg-gray-100 rounded-lg overflow-hidden"
           >
             {renderFile(file)}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        {files.map((file, index) => (
+          <SwiperSlide
+            key={index}
+            className="bg-gray-100 rounded-lg overflow-hidden"
+          >
+            {renderThumbs(file)}
           </SwiperSlide>
         ))}
       </Swiper>
